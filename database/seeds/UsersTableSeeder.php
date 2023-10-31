c2ec5695ed6105eeeaf57ad59bbb7c3d087f9fe0
yuuuki-miyaaa <miyazaki07yuki09@gmail.com>
2023-10-31 20:17:59 +0900

    #ユーザーの新規登録/ログイン処理 #新規登録処理にバリデーション処理 #新規登録完了ページのユーザー名を表示 #ログアウト機能 #トップ/プロフィール/フォロー/フォロワー/検索ページへのリンク設置
    上記項目を実装しました！

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'username' => 'テストユーザー',
+            'mail' => 'test@lull.com',
+            'password' => Hash::make('test'),
+            //DBのusersテーブルに上記値を入れる
+            //Hash::make関数でパスワードを保護(ハッシュ化する)
+        ]);
+    }
+}
